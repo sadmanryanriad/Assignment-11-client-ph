c77a3302c639817b028fa895c9e94a46441d54dd
sadmanryanriad <sadmanryanriad@gmail.com>
2023-11-08 02:34:28 +0600

    show all testimonials in the home page successful

--- src/components/Pages/testimonial/UserTestimonials.jsx
+++ src/components/Pages/testimonial/UserTestimonials.jsx
@@ -1,63 +1,25 @@
 import { useState } from "react";
 import TestimonialCard from "../testimonial/TestimonialCard";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useEffect } from "react";
 
 const UserTestimonials = () => {
   const [expanded, setExpanded] = useState(false);
 
-  const testimonials = [
-    {
-      id: 1,
-      image: "https://i.ibb.co/wybYrpD/event.png",
-      name: "Paul Starr",
-      rating: 5,
-      content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa sit rerum incidunt...",
-    },
-    {
-      id: 2,
-      image: "https://i.ibb.co/wybYrpD/event.png",
-      name: "Jane Doe",
-      rating: 4,
-      content: "Saepe illo est quia obcaecati neque quibusdam eius accusamus error officiis atque voluptates magnam! ",
-    },
-    // Add more initial testimonials here
-    {
-      id: 3,
-      image: "https://i.ibb.co/image3.png",
-      name: "John Smith",
-      rating: 5,
-      content: "The best hotel I've ever stayed in. The staff is friendly, and the rooms are clean and comfortable. Highly recommended!",
-    },
-    {
-      id: 4,
-      image: "https://i.ibb.co/image4.png",
-      name: "Alice Johnson",
-      rating: 4,
-      content: "Had a great experience at this hotel. The location is perfect, and the amenities are excellent.",
-    },
-    {
-      id: 5,
-      image: "https://i.ibb.co/image5.png",
-      name: "David Brown",
-      rating: 5,
-      content: "Outstanding service and beautiful rooms. I'll definitely stay here again on my next trip.",
-    },
-    {
-      id: 6,
-      image: "https://i.ibb.co/image6.png",
-      name: "Sarah White",
-      rating: 4,
-      content: "A pleasant stay with courteous staff. The view from the room was breathtaking.",
-    },
-    {
-      id: 7,
-      image: "https://i.ibb.co/image7.png",
-      name: "Michael Clark",
-      rating: 5,
-      content: "I've been to many hotels, but this one stands out. The food was delicious, and the room was cozy.",
-    },
-  ];
+  const [ratings, setRatings] = useState([]);
+  const axiosSecure = useAxiosSecure();
 
-  const displayedTestimonials = expanded ? testimonials : testimonials.slice(0, 3);
+  const url = `/ratings`;
+  useEffect(()=>{
+    axiosSecure.get(url)
+    .then(res=>{
+      setRatings(res.data);
+    })
+  },[axiosSecure, url])
+
+
+  //how many ratings to show
+  const displayRatings = expanded ? ratings : ratings.slice(0, 3);
 
   return (
     <section className="">
@@ -67,13 +29,13 @@ const UserTestimonials = () => {
         </h2>
 
         <div className="mt-8 grid grid-cols-1 p-2 md:grid-cols-3 gap-6">
-          {displayedTestimonials.map((testimonial) => (
+          {displayRatings.map((testimonial) => (
             <TestimonialCard
-              key={testimonial.id}
-              image={testimonial.image}
-              name={testimonial.name}
-              rating={testimonial.rating}
-              content={testimonial.content}
+              key={testimonial?._id}
+              image={testimonial?.photoURL}
+              name={testimonial?.displayName}
+              rating={parseInt(testimonial?.rating)}
+              content={testimonial?.message}
             />
           ))}
         </div>
@@ -81,7 +43,7 @@ const UserTestimonials = () => {
         <div className="text-center mt-6">
           {expanded ? (
             <button
-              className="btn bg-green-500 text-white hover:bg-gray-600"
+              className=" btn bg-green-400 border-none px-8 py-3 rounded-full hover:bg-gray-600 hover:text-gray-200 hover:scale-105 transform transition duration-300"
               onClick={() => setExpanded(false)}
             >
               See Less
