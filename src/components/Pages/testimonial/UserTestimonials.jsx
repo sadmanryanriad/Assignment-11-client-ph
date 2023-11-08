7328c65d81241135e86df68b0fe03b8e072313ae
sadmanryanriad <sadmanryanriad@gmail.com>
2023-11-08 23:12:09 +0600

    user testimonials tab responsiveness issue fixed

--- src/components/Pages/testimonial/UserTestimonials.jsx
+++ src/components/Pages/testimonial/UserTestimonials.jsx
@@ -29,7 +29,7 @@ const UserTestimonials = () => {
           User Testimonials
         </h2>
 
-        <div className="mt-8 grid grid-cols-1 p-2 md:grid-cols-3 gap-6">
+        <div className="mt-8 grid grid-cols-1 p-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {displayRatings.map((testimonial) => (
             <TestimonialCard
               key={testimonial?._id}
