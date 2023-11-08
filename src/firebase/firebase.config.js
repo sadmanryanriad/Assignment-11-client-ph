c2865e2308b733638041edc1060847883260a3d4
sadmanryanriad <sadmanryanriad@gmail.com>
2023-11-08 23:35:48 +0600

    environment variable for firebase added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAGFWJc-pk5h2gA4WG9Ds7Si5ZHNBqGGMU",
-  authDomain: "assignment-11-785f0.firebaseapp.com",
-  projectId: "assignment-11-785f0",
-  storageBucket: "assignment-11-785f0.appspot.com",
-  messagingSenderId: "905508589275",
-  appId: "1:905508589275:web:1212c715faab2d3c0346fe"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
