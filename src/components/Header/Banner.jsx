587c7860da8a4804fbe69aa42207003858f9192d
sadmanryanriad <sadmanryanriad@gmail.com>
2023-11-09 11:50:10 +0600

    parallax effect added to banner

--- src/components/Header/Banner.jsx
+++ src/components/Header/Banner.jsx
@@ -1,10 +1,9 @@
 import { Link } from "react-router-dom";
-import { useAos } from "../../hooks/useAos";
 import Typed from "typed.js";
 import { useEffect } from "react";
+import { Parallax } from "react-parallax";
 
 const Banner = () => {
-  useAos({ offset: 200, duration: 600, easing: "ease-in-sine", delay: 100 });
 
   useEffect(() => {
     const options = {
@@ -29,18 +28,13 @@ const Banner = () => {
     };
   }, []);
 
+  //https://pixabay.com/photos/pool-spa-wellness-hotel-resort-384573/
+
   return (
-    <div
-      className="relative bg-cover bg-center h-[80vh]"
-      data-aos="zoom-in-down"
+<Parallax strength={500} bgImage="https://i.ibb.co/44qGBgR/pool-384573-1920.jpg" >
+<div
+      className="relative bg-cover bg-center h-[80vh] md:h-screen"
     >
-      {/* Background Image */}
-      <div
-        className="absolute top-0 left-0 w-full h-full bg-cover bg-center"
-        style={{
-          backgroundImage: "url(https://i.ibb.co/44qGBgR/pool-384573-1920.jpg)",
-        }}
-      ></div>
 
       {/* Semi-transparent Overlay */}
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-10 dark:opacity-30"></div>
@@ -52,18 +46,14 @@ const Banner = () => {
             <span className="typing-element"></span>
           </h1>
           <Link to="/rooms">
-            <button className="mt-40 hidden md:block mx-auto btn bg-green-400 border-none px-8 py-3 rounded-full hover:bg-gray-800 hover:text-gray-200 hover:scale-105 transform transition duration-300">
+            <button className="mt-40 mx-auto btn bg-green-400 border-none px-8 py-3 rounded-full hover:bg-gray-800 hover:text-gray-200 hover:scale-105 transform transition duration-300">
               Explore Rooms
             </button>
           </Link>
         </div>
       </div>
-      <Link to={"/rooms"}>
-        <button className="fixed md:hidden bottom-10 left-1/2 transform -translate-x-1/2 btn bg-green-400 border-none px-8 py-3 rounded-full hover:bg-gray-800 hover:text-gray-200 hover:scale-105 transition duration-300">
-          Explore Rooms
-        </button>
-      </Link>
     </div>
+</Parallax>
   );
 };
 
